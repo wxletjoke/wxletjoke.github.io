ca864bb5441604f23e0e83217a6ee4007611b724
hongzhiscau@qq.com <hongzhiscau@qq.com>
2017-02-21 18:15:29 +0800

    change charset to utf-8

--- images/top.js
+++ images/top.js
@@ -9,7 +9,7 @@ document.writeln('<DIV  style="z-index:9;right:0;bottom:0;height:'+h+'px;width:'
 else {
 document.writeln('<DIV style="z-index:9;right:0;bottom:0;height:'+h+'px;width:'+w+'px;overflow:hidden;POSITION:fixed;*position:absolute;*top:expression(eval(document.body.scrollTop)+eval(document.body.clientHeight)-this.style.pixelHeight);">');
 }
-document.writeln('<div style="clear:both;margin:auto;height:210px;font-size:12px;overflow:hidden;text-align:left;"><a href="http://my.joke.wxlet.com/shortcut.php"><font color="#FF0000">ÏÂÔØµ½×ÀÃæ</font></a><br /><br /><a href="javascript:scroll(0,0)" hidefocus="true">¡ü¶¥²¿</a><div class="blank15"></div><a href="javascript:window.close()">¡Á</a><div class="blank15"></div><a href="javascript:scroll(0,99999)" hidefocus="true">µ×²¿¡ı</a></div> ');
+document.writeln('<div style="clear:both;margin:auto;height:210px;font-size:12px;overflow:hidden;text-align:left;"><a href="http://my.joke.wxlet.com/shortcut.php"><font color="#FF0000">ä¸‹è½½åˆ°æ¡Œé¢</font></a><br /><br /><a href="javascript:scroll(0,0)" hidefocus="true">â†‘é¡¶éƒ¨</a><div class="blank15"></div><a href="javascript:window.close()">Ã—</a><div class="blank15"></div><a href="javascript:scroll(0,99999)" hidefocus="true">åº•éƒ¨â†“</a></div> ');
 document.write('<div style="clear:both;margin:auto;overflow:hidden;text-align:left;">'+str+'</div>');
 document.writeln('</DIV>');
 window.onerror=function(){return true;}
